666b938611e2721144229896fe76bb91750d7033
aryan22csu031 <aryan22csu031@ncuindia.edu>
2025-04-01 22:26:06 +0530

    configure MongoDB connection; remove unused Login and Signup components; update server setup

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+const connectDB = require("./config/db.js");
+const petRoutes = require("./routes/pet.routes.js");
+const cookieParser = require("cookie-parser");
+
+require('dotenv').config();
+connectDB();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser);
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(err => console.log(err));
+app.use("/api/pets", petRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
