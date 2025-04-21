13bca038cf7af1c3b227c3c7719a8101947fec8b
aryan22csu031 <aryan22csu031@ncuindia.edu>
2025-04-21 12:07:39 +0530

    fix: update CORS origin to use production URL for frontend access

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", // your frontend origin
+    origin: "https://virtual-pet-assistant-client.onrender.com",
     credentials: true, // allow cookies
   })
 );
